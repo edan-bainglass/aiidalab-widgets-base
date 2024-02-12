edffbc01175876b651e8439e9562422e2c451fa4
Edan Bainglass <edan.bainglass@gmail.com>
2024-02-12 08:18:02 +0000

    Move AiiDA query options to service

--- aiidalab_widgets_base/query_builder/service.py
+++ aiidalab_widgets_base/query_builder/service.py
@@ -3,3 +3,41 @@ from __future__ import annotations
 
 class AiiDAService:
     """docstring"""
+
+
+NODE_RELATIONSHIPS = [
+    "",
+    "outgoing",
+    "incoming",
+    "group",
+]
+
+GROUP_RELATIONSHIPS = [
+    "",
+    "node",
+    "user",
+]
+
+JOINS = [
+    "and",
+    "or",
+]
+
+OPERATORS = [
+    "==",
+    "in",
+    ">",
+    "<",
+    "<=",
+    ">=",
+    "like",
+    "ilike",
+    "or",
+    "and",
+    "has_key",
+    "of_type",
+    "of_length",
+    "shorter",
+    "longer",
+    "contains",
+]
